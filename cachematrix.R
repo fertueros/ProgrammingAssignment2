14fd1c158ce042b6a2d3795ea15cd1f4d6a472c1
fertueros <fertueros@gmail.com>
2014-10-24 10:58:08 -0500

    the second updating

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 ## Sorry for my english
-## My function prepare a structure for a matrix. Then, calculate the inverse of matrix with "solve".
-## Save the answer in cache of my computer and return that, if I call that.
-## I assume that matrix is square and has inverse.
+## My function prepare a structure for a matrix. Take the answer of the inverse when is calculated.
+
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -22,7 +21,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## Save the answer in cache of my computer and return that, if I call that.
+## Calculate the inverse of matrix with "solve".
+## I assume that matrix is square and has inverse.
 cacheSolve <- function(x) {
-  
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+    m  
 }
